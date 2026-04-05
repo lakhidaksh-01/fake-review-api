d981445a4c1bc9832f2bd9d5cfa419dd1416b334
DAKSH LAKHI <dakshlakhi01@gmail.com>
2026-04-05 17:21:03 +0530

    ml connect

--- services/mlService.js
+++ services/mlService.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 exports.analyze = async (text) => {
   try {
     const response = await axios.post(
-       "http://127.0.0.1:8000/predict",
+       "https://fake-review-ml-service.onrender.com/predict",
       { text }
     );
 
