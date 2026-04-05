3e28c2a3319bcf626071c0d215bc439765a52844
DAKSH LAKHI <dakshlakhi01@gmail.com>
2026-04-05 18:58:20 +0530

    #

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
   res.send("Backend is running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
